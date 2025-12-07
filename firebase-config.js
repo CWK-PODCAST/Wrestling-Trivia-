007307d7668e89f0dffff5a39fb83a5a717cfea4
CWK-PODCAST <cruisingwithkayfabe@gmail.com>
2025-12-06 20:34:19 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww37900\viewh21300\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 // firebase-config.js\
+// Paste this file into the project root (same folder as game.html, control.html, buzzer.html)\
+\
+// THIS IS YOUR FIREBASE CONFIG (as provided)\
+const FIREBASE_CONFIG = \{\
+  apiKey: "AIzaSyCrVqobGU3iHURKtg0xnJgC6vyczPsuFAg",\
+  authDomain: "wrestling-trvia.firebaseapp.com",\
+  projectId: "wrestling-trvia",\
+  storageBucket: "wrestling-trvia.firebasestorage.app",\
+  messagingSenderId: "563113895024",\
+  appId: "1:563113895024:web:e9ba6b80f074aee32840b5",\
+  measurementId: "G-C48QSLD94B"\
+\};\
+\
+}
